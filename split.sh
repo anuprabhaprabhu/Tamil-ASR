df2acc8318767c5da0ea50ebabb3059eef13fda8
anuprabhaprabhu <76058828+anuprabhaprabhu@users.noreply.github.com>
2022-12-30 14:03:57 +0530

    Add files via upload

--- split.sh
+++ split.sh
@@ -1,8 +1,8 @@
- #! /usr/bin/bash
+#! /usr/bin/bash
 
-src_fld=$1
+src_fld=$1    #give absolute path --- important
 src_fmt=$2
-dest_fld=$3
+dest_fld=$3   #create this file in the folder
 
 for file in $(find $src_fld -type f -name "*.$src_fmt"); do
     file_name=$(basename "${file}")
